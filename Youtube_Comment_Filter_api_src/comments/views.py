d2cb10e3d13d071e95d243137261df58f8952bd0
Jesse Kanter <jessekanter@gmail.com>
2019-11-20 10:38:35 -0500

    added start and duration input

--- Youtube_Comment_Filter_api_src/comments/views.py
+++ Youtube_Comment_Filter_api_src/comments/views.py
@@ -3,39 +3,46 @@ from django.shortcuts import render
 import numpy as np
 import pandas as pd
 
-from extractor.views import get_authenticated_service, get_comments_df, select_script_transcript
+from extractor.views import get_comments_df, get_transcript_df, select_script_transcript
 from vectorizer.views import bow_transformer, tf_idf_transformer, tf_idf_transform
 from sklearn.metrics.pairwise import cosine_similarity
 
 
 from django.http import HttpResponse
 import json
+from time import time
 # Create your views here.
 
 
 
 def predicted_df(start_time,add_time,comments,tran):
-    
+
     Bow_transformer = bow_transformer(tran)
-    Tf_idf_transformer = tf_idf_transformer(df,Bow_transformer)
-    
-    tran_string_select=tf_idf_transform(select_script_transcript(start,duration,tran),Tf_idf_transformer,Bow_transformer)
-    
+    Tf_idf_transformer = tf_idf_transformer(tran,Bow_transformer)
+
+    tran_string_select=tf_idf_transform(select_script_transcript(start_time,add_time,tran),Tf_idf_transformer,Bow_transformer)
+
     comments['sim_score']=comments.Comment.apply(lambda s: cosine_similarity(tran_string_select, tf_idf_transform(s,Tf_idf_transformer,Bow_transformer) )[0][0] )
-    
+
     return comments.sort_values(by='sim_score',ascending=False).head(3)
 
 
-def comments(request):
+def comments(request):   #request
     video_id=request.GET['video_id']
+    video_id=video_id.split('&')[0]
+    video_id=video_id.split('=')[0]
     tran= get_transcript_df(video_id)
     comments= get_comments_df(video_id)
 
-    comments_relevant_df = predicted_df(0,30,comments,tran)
+    tic = time()
+
+    start=int(request.GET['start'])#
+    duration=int(request.GET['duration'])#
+    comments_relevant_df = predicted_df(start, duration,comments,tran)
     comment_list = []
 
     # if recommendations are found, find events in the list and construct the response dict
-    if len(comments_df) != 0:
+    if len(comments_relevant_df) != 0:
         found = True
         i=0
         while i < len(comments_relevant_df):
@@ -44,10 +51,15 @@ def comments(request):
             comment_likes = comments_relevant_df.iloc[i].Likes
             comment_number_replies = comments_relevant_df.iloc[i].Replies
             comment_text = comments_relevant_df.iloc[i].Comment
+            comment_author = comments_relevant_df.iloc[i].Author
+            comment_author_link = comments_relevant_df.iloc[i].Author_link
+
 
             comment_list.append({
-                'likes' : comment_likes,
-                'Replies' : comment_number_replies,
+                'likes' : str(comment_likes),
+                'Replies' : str(comment_number_replies),
+                'Author' : str(comment_author),
+                'Author_link' : str(comment_author_link),
                 'comment_text' : comment_text
             })
             i+=1
@@ -60,8 +72,9 @@ def comments(request):
     response = json.dumps(comment_dict)
 
     print("Time lapse {}".format(time() - tic))
-
     return HttpResponse(response)
 
+##def index(request):
+##    return HttpResponse("Hello, world. testing for comments")
 
     
